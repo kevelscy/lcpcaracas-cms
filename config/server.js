e57f58f2ab1bc0ad0a00a536c1392534426d5fa2
ScriptKev <script.kev@gmail.com>
2021-08-06 14:27:35 -0400

    init: nueva version de lcpcaracas-cms

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '4cca62c24543f96f396a2c92435546e8'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'cd411d4f-42e6-416e-b175-0182b9f23408'
+};
